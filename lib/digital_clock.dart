1f3ba4535c2d89cc45c1d1165d36692881402655
Oguzhan <oguzhan824@gmail.com>
2020-01-06 22:27:06 +0300

    added location, temp, weather fields and editing theme

--- lib/digital_clock.dart
+++ lib/digital_clock.dart
@@ -3,26 +3,25 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+import 'dart:ui';
 
 import 'package:flutter/services.dart';
 import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 
-enum _Element { background, hourText, minuteText, hourShadow, minuteShadow }
+enum _Element { background, text, shadow }
 
 final _lightTheme = {
   _Element.background: Colors.white,
-  _Element.hourText: Color.fromRGBO(0, 5, 33, 1),
-  _Element.minuteText: Color.fromRGBO(255, 0, 170, 1),
+  _Element.text: Color.fromRGBO(0, 5, 33, 1),
+  _Element.shadow: Colors.red,
 };
 
 final _darkTheme = {
   _Element.background: Color.fromRGBO(0, 5, 33, 1),
-  _Element.hourText: Colors.white,
-  _Element.minuteText: Color.fromRGBO(255, 0, 170, 1),
-  _Element.hourShadow: Colors.white,
-  _Element.minuteShadow: Color.fromRGBO(255, 0, 170, 1),
+  _Element.text: Colors.white,
+  _Element.shadow: Color(0xFF00AA),
 };
 
 /// A basic digital clock.
@@ -66,6 +65,12 @@ class _DigitalClockState extends State<DigitalClock> {
     _timer?.cancel();
     widget.model.removeListener(_updateModel);
     widget.model.dispose();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     super.dispose();
   }
 
@@ -100,36 +105,133 @@ class _DigitalClockState extends State<DigitalClock> {
     final colors = Theme.of(context).brightness == Brightness.light
         ? _lightTheme
         : _darkTheme;
+    final location = widget.model.location;
+    final temp = widget.model.temperatureString;
+    final weather = widget.model.weatherString;
     final hour =
         DateFormat(widget.model.is24HourFormat ? 'HH' : 'hh').format(_dateTime);
     final minute = DateFormat('mm').format(_dateTime);
-    final fontSize = MediaQuery.of(context).size.width / 3.5;
-    final offset = -fontSize / 7;
+    final fontSize = MediaQuery.of(context).size.width / 4;
     final defaultStyle = TextStyle(
-      color: colors[_Element.text],
-      fontFamily: 'PressStart2P',
-      fontSize: fontSize,
-      shadows: [
-        Shadow(
-          blurRadius: 0,
-          color: colors[_Element.shadow],
-          offset: Offset(10, 0),
-        ),
-      ],
-    );
+        color: colors[_Element.text],
+        fontFamily: 'Montserrat',
+        fontWeight: FontWeight.w300,
+        fontSize: fontSize,
+        shadows: [Shadow(color: colors[_Element.shadow])]);
 
     return Container(
       color: colors[_Element.background],
       child: Center(
         child: DefaultTextStyle(
-          style: defaultStyle,
-          child: Stack(
-            children: <Widget>[
-              Positioned(left: offset, top: 0, child: Text(hour)),
-              Positioned(right: offset, bottom: offset, child: Text(minute)),
-            ],
-          ),
-        ),
+            style: defaultStyle,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      hour + ":",
+                      style: TextStyle(
+                          color: colors[_Element.text],
+                          fontWeight: FontWeight.w200,
+                          shadows: [
+                            Shadow(
+                                color: Colors.white,
+                                blurRadius: 10,
+                                offset: Offset(0, 3.0)),
+                          ]),
+                    ),
+                    Text(minute,
+                        style: TextStyle(
+                          fontWeight: FontWeight.w200,
+                          color: Color.fromRGBO(255, 0, 170, 1),
+                          shadows: [
+                            Shadow(
+                                color: (colors == _lightTheme)
+                                    ? Colors.transparent
+                                    : colors[_Element.shadow],
+                                blurRadius: 10,
+                                offset: Offset(0, 3.0))
+                          ],
+                        ))
+                  ],
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 2,
+                                    color: Color.fromRGBO(255, 0, 170, 1)),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(3))),
+                            child: Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Text(
+                                location.toUpperCase(),
+                                style: TextStyle(
+                                    fontSize: 20, color: colors[_Element.text]),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(top: 15, bottom: 10),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          Padding(
+                            padding: const EdgeInsets.only(right: 10),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                  border: Border.all(
+                                      width: 2,
+                                      color: Color.fromRGBO(255, 0, 170, 1)),
+                                  borderRadius:
+                                      BorderRadius.all(Radius.circular(3))),
+                              child: Padding(
+                                padding: const EdgeInsets.all(5.0),
+                                child: Text(
+                                  weather.toUpperCase(),
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      color: colors[_Element.text]),
+                                ),
+                              ),
+                            ),
+                          ),
+                          Container(
+                            decoration: BoxDecoration(
+                                border: Border.all(
+                                    width: 2,
+                                    color: Color.fromRGBO(255, 0, 170, 1)),
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(3))),
+                            child: Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: Text(
+                                temp.toUpperCase(),
+                                style: TextStyle(
+                                    fontSize: 20, color: colors[_Element.text]),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    )
+                  ],
+                )
+              ],
+            )),
       ),
     );
   }
